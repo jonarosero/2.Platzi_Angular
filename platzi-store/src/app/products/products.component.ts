6f180bbe8f121618d03242c86bee108b2e70a322
jonarosero <jars9605@hotmail.com>
2020-06-14 12:34:14 -0500

    create banner and not found page

--- platzi-store/src/app/products/products.component.ts
+++ platzi-store/src/app/products/products.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Product } from '../product.model';
+import { Product } from './../product.model';
 
 @Component({
   selector: 'app-products',
@@ -13,42 +13,42 @@ export class ProductsComponent implements OnInit {
     {
       id: '1',
       title: 'Camiseta',
-      img: 'https://images.unsplash.com/photo-1577212017308-55c4d60d2609?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      img: '../assets/images/camiseta.png',
       price: 8000,
       description: 'blablablalba'
     },
     {
       id: '2',
       title: 'Hoodie',
-      img: 'https://images.unsplash.com/photo-1556821840-3a63f95609a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      img: '../assets/images/hoodie.png',
       price: 8000,
       description: 'blablablalba'
     },
     {
       id: '3',
       title: 'Mug',
-      img: 'https://images.unsplash.com/photo-1481973946307-512988dde8b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      img: '../assets/images/mug.png',
       price: 8000,
       description: 'blablablalba'
     },
     {
       id: '4',
       title: 'Pin',
-      img: 'https://images.unsplash.com/photo-1569803237283-4af155d9e52b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      img: '../assets/images/pin.png',
       price: 8000,
       description: 'bllkjlkjokj'
     },
     {
       id: '5',
       title: 'Stickers',
-      img: 'https://images.unsplash.com/photo-1573434490486-3cab65054136?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      img: '../assets/images/stickers1.png',
       price: 8000,
       description: 'bllkjlkjokj'
     },
     {
       id: '6',
-      title: 'Console',
-      img: 'https://images.unsplash.com/photo-1545958090-5a8a3589718a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      title: 'Stickers',
+      img: '../assets/images/stickers2.png',
       price: 8000,
       description: 'bllkjlkjokj'
     },
